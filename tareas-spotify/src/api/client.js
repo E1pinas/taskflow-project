adf36a114fd7687f5cf141db144c0222abfd9836
E1Pinas <wadeider7x7@gmail.com>
2026-03-24 12:53:41 +0100

    Tercara prueba de backend

--- tareas-spotify/src/api/client.js
+++ tareas-spotify/src/api/client.js
@@ -1,27 +1,11 @@
-const LOCAL_API_BASE_URL = "http://localhost:3000/api/v1/tasks";
+const LOCAL_API_BASE_URL = "/api/v1/tasks";
 
-function getConfiguredApiBaseUrl() {
-  const htmlConfig = document.documentElement.dataset.apiBaseUrl?.trim();
 
-  if (htmlConfig) {
-    return htmlConfig;
-  }
 
-  const isLocalHost =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1";
 
-  if (isLocalHost) {
-    return LOCAL_API_BASE_URL;
-  }
-
-  return `${window.location.origin}/api/v1/tasks`;
-}
-
-const API_BASE_URL = getConfiguredApiBaseUrl();
 
 async function request(path = "", options = {}) {
-  const response = await fetch(`${API_BASE_URL}${path}`, {
+  const response = await fetch(`${LOCAL_API_BASE_URL}${path}`, {
     headers: {
       "Content-Type": "application/json",
       ...(options.headers || {}),
