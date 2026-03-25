75b92b50ffb614a8cd6f5c2686ac091dac433346
E1Pinas <wadeider7x7@gmail.com>
2026-03-25 11:57:40 +0100

    cambios en parte 3 de server

--- tareas-spotify/server.js
+++ tareas-spotify/server.js
@@ -1,3 +1,8 @@
+import express from "express";
 import app from "./server/src/app.js";
+
+// Vercel detects Express from a supported entrypoint that imports
+// the package and exports the app without starting a listener.
+void express;
 
 export default app;
